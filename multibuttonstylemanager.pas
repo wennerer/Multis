817874b9bd1b634316c0bac333d41884e2fa8f7c
wennerer <wennerer@gmail.com>
2022-01-06 09:59:38 +0100

    gcMosaic mit gcAlternate ersetzt

--- multibuttonstylemanager.pas
+++ multibuttonstylemanager.pas
@@ -49,7 +49,7 @@ type
                   //Position MessageButton
 
  type
-   TGradientCourse = (gcHorizontal,gcVertical,gcSpread,gcRadiant,gcMosaic); //for background color
+   TGradientCourse = (gcHorizontal,gcVertical,gcSpread,gcRadiant,gcAlternate); //for background color
 
 
 
