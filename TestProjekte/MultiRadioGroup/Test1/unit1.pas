291c3a0395cddc72f790212ae95067b724bc92f1
wennerer <wennerer@gmail.com>
2023-01-19 21:20:59 +0100

    test projekt begonnen1

---
+++ TestProjekte/MultiRadioGroup/Test1/unit1.pas
@@ -0,0 +1,31 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, MultiRadioGroup;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MultiRadioGroup1: TMultiRadioGroup;
+    MultiRadioGroup2: TMultiRadioGroup;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
