7ebd3f9d26d97d989dfc787b945b7c41c5208465
wennerer <wennerer@gmail.com>
2023-06-02 17:02:22 +0200

    MEL TestProjekt begonnen

--- TestProjekte/MultiEventLine/Test 1/unit1.pas
+++ TestProjekte/MultiEventLine/Test 1/unit1.pas
@@ -5,17 +5,28 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, MultiPanel,
-  MultiEventLine, MultiRadioGroup, MultiButton;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ColorBox, MultiPanel,
+  MultiEventLine, MultiRadioGroup, MultiButton, MultiButtonStyleManager,
+  MultiSeperator;
 
 type
 
-  { TForm1 }
+  { TMainForm }
 
-  TForm1 = class(TForm)
+  TMainForm = class(TForm)
+    ColorBox1: TColorBox;
+    ColorBox2: TColorBox;
+    ImageList1: TImageList;
     MultiButton1: TMultiButton;
+    MultiButton2: TMultiButton;
+    MultiButton3: TMultiButton;
+    MultiButtonStyleManager1: TMultiButtonStyleManager;
     MultiEventLine1: TMultiEventLine;
     MultiPanel1: TMultiPanel;
+    MultiPanel2: TMultiPanel;
+    MultiPanel3: TMultiPanel;
+    MultiSeperator1: TMultiSeperator;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -23,11 +34,22 @@ type
   end;
 
 var
-  Form1: TForm1;
+  MainForm: TMainForm;
 
 implementation
 
 {$R *.lfm}
 
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+ Left := (Monitor.Width div 2) - (Width div 2);
+ Top  := (Monitor.Height div 2) - (Height div 2);
+end;
+
+
+
+{$Include drawing.inc}
 end.
 
