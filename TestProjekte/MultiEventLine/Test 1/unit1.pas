069eb113c104c3f1708f326774dce8e4c9be4cbb
wennerer <wennerer@gmail.com>
2023-06-03 06:04:28 +0200

    MEL über DemoProjekt

--- TestProjekte/MultiEventLine/Test 1/unit1.pas
+++ TestProjekte/MultiEventLine/Test 1/unit1.pas
@@ -5,9 +5,14 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ColorBox, MultiPanel,
-  MultiEventLine, MultiRadioGroup, MultiButton, MultiButtonStyleManager,
-  MultiSeperator;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ColorBox, Spin,
+  MultiPanel, MultiEventLine, MultiRadioGroup, MultiButton,
+  MultiButtonStyleManager, MultiSeperator, MultiplexSlider, MultiCheckGroup;
+
+const
+  Line        = 0;
+  Rectangle   = 1;
+  Polygone    = 2;
 
 type
 
@@ -25,10 +30,33 @@ type
     MultiPanel1: TMultiPanel;
     MultiPanel2: TMultiPanel;
     MultiPanel3: TMultiPanel;
+    MultiPanel4: TMultiPanel;
+    MultiRadioGroup1: TMultiRadioGroup;
     MultiSeperator1: TMultiSeperator;
+    SpinEdit1: TSpinEdit;
+    procedure ColorBox1Change(Sender: TObject);
+    procedure ColorBox2Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDblClick(Sender: TObject);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+    procedure FormMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure FormPaint(Sender: TObject);
+    procedure MultiButtonClick(Sender: TObject);
+    procedure SpinEdit1Change(Sender: TObject);
   private
-
+    PenColor   : TColor;
+    PenWidth   : integer;
+    BrushColor : TColor;
+    draw       : boolean;
+    Choice     : integer;
+    sx,sy      : integer;
+    ex,ey      : integer;
+    Points     : array of TPoint;
+    count      : integer;
+    start      : boolean;
   public
 
   end;
@@ -46,7 +74,19 @@ procedure TMainForm.FormCreate(Sender: TObject);
 begin
  Left := (Monitor.Width div 2) - (Width div 2);
  Top  := (Monitor.Height div 2) - (Height div 2);
+ Choice     := 0; draw := false;
+ PenColor   := clBlue;
+ PenWidth   := 1;
+ BrushColor := clLime;
+ ColorBox1.Selected := clBlue;
+ ColorBox2.Selected := clLime;
+ count := 1;start := false;
+
 end;
+
+
+
+
 
 
 
